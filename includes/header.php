e833ed8d07169fd492384a625af8b847303f59b7
Lojain Alghareeb <alghareeblojain@gmail.com>
2026-07-01 02:05:50 +0300

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<header class="topbar">
+
+    <div class="wrap">
+
+        <a href="index.php" class="logo">
+    Olivian Scent
+</a>
+        <nav class="nav-links">
+
+            <a href="index.php">Home</a>
+
+            <a href="contact.php">Contact</a>
+
+            <a href="cart.php">🛒</a>
+
+        </nav>
+
+    </div>
+
+</header>
